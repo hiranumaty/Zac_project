e8b31676bd2c6f328d2e89a590a6a6fba4c23746
hiranumaty <hiranumaty@noar.co.jp>
2021-11-08 13:56:34 +0900

    見事ダウンロードに成功

--- Zac_first.py
+++ Zac_first.py
@@ -10,22 +10,25 @@ async def ZAC_login(page,USERNAME,PASSWORD):
 
 async def GETCASTING(page):
     await page.click('.menu-trigger')
-    await page.wait_for_selector('a[href="/noar_test/b/output"]')
     await page.click('a[href="/noar_test/b/output"]')
-    await page.wait_for_selector(":nth-match(a:has-text('キャスティング一覧'),2)")
     await page.click(":nth-match(a:has-text('キャスティング一覧'),2)")
     print(page.url)
     #ここでCSVボタンを押下してダウンロードを実行する
-    await page.wait_for_selector('//html/body/form[2]/table/tbody/tr/td/table.bor_a/tbody/tr[4]/td[5]/input.btn_red')
-    #await page.wait_for_selector('xpath=//html/body/form[2]/table/tbody/tr/td/table[3]/tbody/tr[4]/td[5]/input[2]')
-    #await page.click('xpath=//html/body/form[2]/table/tbody/tr/td/table[3]/tbody/tr[4]/td[5]/input[2]')
+    classic_window = await (await page.query_selector("#classic_window")).content_frame()
+    #CSSセレクタ JSセレクタ　full_Xpathすべてなぜか動かない
+    async with page.expect_download() as download_info:
+        await classic_window.click("input.btn_red[name='CSV']", force=True)
+    download = await download_info.value
+    path = download.suggested_filename
+    await download.save_as(path)
 
 async def main():
     USERNAME = "hisadakn"
     PASSWORD = "e-m1mark3"
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
-        page = await browser.new_page()
+        context = await browser.new_context(accept_downloads=True)
+        page = await context.new_page()
         await ZAC_login(page,USERNAME,PASSWORD)
         await GETCASTING(page)
         await browser.close()
